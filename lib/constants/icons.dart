aa4b8fd4caf2dacbc54eb2f8df466898436d52bb
Raunak <raunak51299@gmail.com>
2023-02-24 20:51:42 +0530

    init

---
+++ lib/constants/icons.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+// these are the initial category names and the icons for them
+Map<String, IconData> icons = {
+  'Auto And Transport': Icons.directions_car,
+  'Food And Drinks': Icons.fastfood,
+  'Sports': Icons.sports_soccer,
+  'Entertainment': Icons.movie,
+  'Education': Icons.school,
+  'Other': Icons.miscellaneous_services,
+};
